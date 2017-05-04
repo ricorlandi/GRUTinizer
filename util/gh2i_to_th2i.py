8cb3e5baf53a0dce2faa855560c5752a28cc1270
Chris Sullivan <sullivan@nscl.msu.edu>
2017-05-04 13:29:32 -0400

    rolling back change to upstream python script.

--- util/gh2i_to_th2i.py
+++ util/gh2i_to_th2i.py
@@ -1,13 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/env grutinizer-python
 
 import glob
 import os
-import ROOT
-ROOT.gSystem.Load("/projects/ceclub/sullivan/cagragr/e441/lib/libAllGrutinizer")
 import sys
-sys.path.append('./util/rcnp')
-from pretty_matplotlib import *
 
+import ROOT
 
 def convert(input_filename, output_filename):
     tf_in = ROOT.TFile(input_filename)
@@ -16,17 +13,13 @@ def convert(input_filename, output_filename):
         obj = key.ReadObj()
         if isinstance(obj, ROOT.GH2I):
             output = ROOT.TH2I(obj)
-        elif isinstance(obj, ROOT.GH2D):
-            output = ROOT.TH2D(obj)
-        elif isinstance(obj, ROOT.GH1D):
-            output = explicit_GH1D_to_TH1D(obj)
         else:
             output = obj
 
         if not isinstance(obj, ROOT.TTree):
             output.Write()
 
-for filename in sys.argv[1:]:
+for filename in sys.argv[2:]:
     base = os.path.splitext(filename)[0]
     output = base + '_th2i.root'
     print 'Converting {} to {}'.format(filename, output)
